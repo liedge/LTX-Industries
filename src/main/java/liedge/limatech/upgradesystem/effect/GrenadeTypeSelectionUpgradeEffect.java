ef1a7fe2c89737518886463cf7e2cc9aa44efa31
Ed J <cuttingedge237@gmail.com>
2024-12-19 13:54:18 -0600

    0.8.1 Fixes datagen issue with new codecs

--- src/main/java/liedge/limatech/upgradesystem/effect/GrenadeTypeSelectionUpgradeEffect.java
+++ src/main/java/liedge/limatech/upgradesystem/effect/GrenadeTypeSelectionUpgradeEffect.java
@@ -2,35 +2,36 @@ package liedge.limatech.upgradesystem.effect;
 
 import com.google.common.base.Preconditions;
 import com.mojang.serialization.MapCodec;
-import it.unimi.dsi.fastutil.objects.ObjectSet;
-import liedge.limacore.data.LimaCoreCodecs;
 import liedge.limatech.item.weapon.GrenadeLauncherWeaponItem;
 import liedge.limatech.lib.weapons.GrenadeType;
 import liedge.limatech.registry.LimaTechEquipmentUpgrades;
 import liedge.limatech.upgradesystem.ItemEquipmentUpgrades;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static liedge.limatech.registry.LimaTechDataComponents.GRENADE_TYPE;
 
 public class GrenadeTypeSelectionUpgradeEffect implements EquipmentUpgradeEffect
 {
-    public static final MapCodec<GrenadeTypeSelectionUpgradeEffect> CODEC = LimaCoreCodecs.objectSetCodec(GrenadeType.CODEC).fieldOf("types").xmap(GrenadeTypeSelectionUpgradeEffect::new, o -> o.grenadeTypes);
+    public static final MapCodec<GrenadeTypeSelectionUpgradeEffect> CODEC = GrenadeType.CODEC.setOf().fieldOf("types").xmap(GrenadeTypeSelectionUpgradeEffect::new, o -> o.grenadeTypes);
 
     public static GrenadeTypeSelectionUpgradeEffect unlockSingle(GrenadeType type)
     {
-        return new GrenadeTypeSelectionUpgradeEffect(ObjectSet.of(type));
+        return new GrenadeTypeSelectionUpgradeEffect(EnumSet.of(type));
     }
 
     public static GrenadeTypeSelectionUpgradeEffect unlockMultiple(GrenadeType... types)
     {
-        return new GrenadeTypeSelectionUpgradeEffect(ObjectSet.of(types));
+        return new GrenadeTypeSelectionUpgradeEffect(EnumSet.copyOf(List.of(types)));
     }
 
-    private final ObjectSet<GrenadeType> grenadeTypes;
+    private final Set<GrenadeType> grenadeTypes;
 
-    private GrenadeTypeSelectionUpgradeEffect(ObjectSet<GrenadeType> grenadeTypes)
+    private GrenadeTypeSelectionUpgradeEffect(Set<GrenadeType> grenadeTypes)
     {
         Preconditions.checkArgument(!grenadeTypes.contains(GrenadeType.EXPLOSIVE), "Grenade type selection upgrade cannot contain Explosive since it is the default element.");
         this.grenadeTypes = grenadeTypes;
