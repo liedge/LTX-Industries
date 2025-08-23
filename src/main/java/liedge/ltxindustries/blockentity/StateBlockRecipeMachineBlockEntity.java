cc0b1c1d1b6c94e395a8d4c728b211c3ca65a7d9
Ed J <cuttingedge237@gmail.com>
2025-08-22 23:09:23 -0500

    Re-organized the primary fluid handler implementation for machines.

--- src/main/java/liedge/ltxindustries/blockentity/StateBlockRecipeMachineBlockEntity.java
+++ src/main/java/liedge/ltxindustries/blockentity/StateBlockRecipeMachineBlockEntity.java
@@ -1,18 +1,15 @@
 package liedge.ltxindustries.blockentity;
 
-import liedge.limacore.LimaCommonConstants;
 import liedge.limacore.blockentity.BlockContentsType;
 import liedge.limacore.capability.energy.LimaEnergyStorage;
 import liedge.limacore.capability.energy.LimaEnergyUtil;
-import liedge.limacore.capability.fluid.FluidHolderBlockEntity;
-import liedge.limacore.capability.fluid.LimaBlockEntityFluidHandler;
-import liedge.limacore.capability.fluid.LimaFluidUtil;
 import liedge.limacore.client.gui.TooltipLineConsumer;
 import liedge.limacore.recipe.LimaRecipeCheck;
-import liedge.limacore.util.LimaRegistryUtil;
-import liedge.ltxindustries.LTXIndustries;
 import liedge.ltxindustries.block.MachineState;
-import liedge.ltxindustries.blockentity.base.*;
+import liedge.ltxindustries.blockentity.base.EnergyConsumerBlockEntity;
+import liedge.ltxindustries.blockentity.base.RecipeMachineBlockEntity;
+import liedge.ltxindustries.blockentity.base.SidedAccessBlockEntityType;
+import liedge.ltxindustries.blockentity.base.VariableTimedProcessBlockEntity;
 import liedge.ltxindustries.blockentity.template.ProductionMachineBlockEntity;
 import liedge.ltxindustries.lib.upgrades.EffectRankPair;
 import liedge.ltxindustries.lib.upgrades.effect.value.ValueUpgradeEffect;
@@ -20,12 +17,9 @@ import liedge.ltxindustries.lib.upgrades.machine.MachineUpgrades;
 import liedge.ltxindustries.registry.game.LTXIUpgradeEffectComponents;
 import liedge.ltxindustries.util.LTXITooltipUtil;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.component.DataComponentType;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.crafting.Recipe;
@@ -35,45 +29,30 @@ import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.neoforged.neoforge.capabilities.BlockCapabilityCache;
-import net.neoforged.neoforge.capabilities.Capabilities;
-import net.neoforged.neoforge.fluids.FluidStack;
-import net.neoforged.neoforge.fluids.capability.IFluidHandler;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.DoubleUnaryOperator;
 
 import static liedge.ltxindustries.block.LTXIBlockProperties.BINARY_MACHINE_STATE;
 
 public abstract class StateBlockRecipeMachineBlockEntity<I extends RecipeInput, R extends Recipe<I>> extends ProductionMachineBlockEntity
-        implements FluidHolderBlockEntity, VariableTimedProcessBlockEntity, EnergyConsumerBlockEntity, RecipeMachineBlockEntity<I, R>
+        implements VariableTimedProcessBlockEntity, EnergyConsumerBlockEntity, RecipeMachineBlockEntity<I, R>
 {
-    public static final int INPUT_TANK_CAPACITY = 32_000;
-    public static final int OUTPUT_TANK_CAPACITY = 64_000;
-
-    private final @Nullable LimaBlockEntityFluidHandler inputFluids;
-    private final @Nullable LimaBlockEntityFluidHandler outputFluids;
-    private final @Nullable IOController fluidController;
-    private final Map<Direction, BlockCapabilityCache<IFluidHandler, Direction>> fluidConnections = new EnumMap<>(Direction.class);
     private final LimaRecipeCheck<I, R> recipeCheck;
-
     private int energyUsage = getBaseEnergyUsage();
     private DoubleUnaryOperator recipeTimeFunction = DoubleUnaryOperator.identity();
     private int recipeCraftingTime;
     private int craftingProgress;
     private boolean crafting;
-    private int autoFluidOutputTimer = 0;
     private boolean shouldCheckRecipe;
     private boolean shouldCheckCraftingTime;
 
     protected StateBlockRecipeMachineBlockEntity(SidedAccessBlockEntityType<?> type, RecipeType<R> recipeType, BlockPos pos, BlockState state, int inputSlots, int outputSlots, int inputTanks, int outputTanks)
     {
-        super(type, pos, state, 2, inputSlots, outputSlots);
+        super(type, pos, state, 2, inputSlots, outputSlots, inputTanks, outputTanks);
         this.recipeCheck = LimaRecipeCheck.create(recipeType);
-        this.inputFluids = inputTanks > 0 ? new LimaBlockEntityFluidHandler(this, inputTanks, BlockContentsType.INPUT) : null;
-        this.outputFluids = outputTanks > 0 ? new LimaBlockEntityFluidHandler(this, outputTanks, BlockContentsType.OUTPUT) : null;
-        this.fluidController = inputFluids != null || outputFluids != null ? new IOController(this, BlockEntityInputType.FLUIDS) : null;
     }
 
     @Override
@@ -174,87 +153,12 @@ public abstract class StateBlockRecipeMachineBlockEntity<I extends RecipeInput, 
 
     // Fluid handler implementation
     @Override
-    public @Nullable LimaBlockEntityFluidHandler getFluidHandler(BlockContentsType contentsType)
-    {
-        return switch (contentsType)
-        {
-            case INPUT -> inputFluids;
-            case OUTPUT -> outputFluids;
-            default -> null;
-        };
-    }
-
-    @Override
-    public int getBaseFluidCapacity(BlockContentsType contentsType, int tank)
-    {
-        return contentsType == BlockContentsType.INPUT ? INPUT_TANK_CAPACITY : OUTPUT_TANK_CAPACITY;
-    }
-
-    @Override
-    public int getBaseFluidTransferRate(BlockContentsType contentsType, int tank)
-    {
-        return getBaseFluidCapacity(contentsType, tank) / 4;
-    }
-
-    @Override
-    public boolean isValidFluid(BlockContentsType contentsType, int tank, FluidStack stack)
-    {
-        return true;
-    }
-
-    @Override
-    public @Nullable IFluidHandler createFluidIOWrapper(@Nullable Direction side)
-    {
-        return wrapInputOutputTanks(side);
-    }
-
-    @Override
-    protected IOController getFluidIOController()
-    {
-        return fluidController != null ? fluidController : super.getFluidIOController();
-    }
-
-    @Override
     public void onFluidsChanged(BlockContentsType contentsType, int tank)
     {
-        setChanged();
+        super.onFluidsChanged(contentsType, tank);
         if (contentsType == BlockContentsType.INPUT || (contentsType == BlockContentsType.OUTPUT && !crafting))
         {
             shouldCheckRecipe = true;
-        }
-    }
-
-    @Override
-    public @Nullable IFluidHandler getNeighborFluidHandler(Direction side)
-    {
-        return fluidConnections.get(side).getCapability();
-    }
-
-    private void autoOutputFluids()
-    {
-        if (outputFluids != null && fluidController != null && fluidController.isAutoOutput())
-        {
-            for (Direction side : Direction.values())
-            {
-                if (fluidController.getSideIOState(side).allowsOutput())
-                {
-                    IFluidHandler neighborFluids = getNeighborFluidHandler(side);
-                    if (neighborFluids != null) LimaFluidUtil.transferFluidsFromLimaTank(outputFluids, neighborFluids, OUTPUT_TANK_CAPACITY, IFluidHandler.FluidAction.EXECUTE);
-                }
-            }
-        }
-    }
-
-    private void autoOutputFluids(int frequency)
-    {
-        if (autoFluidOutputTimer >= frequency)
-        {
-            autoOutputFluids();
-            autoFluidOutputTimer = 0;
-        }
-        else
-        {
-            autoFluidOutputTimer++;
         }
     }
 
@@ -288,7 +192,6 @@ public abstract class StateBlockRecipeMachineBlockEntity<I extends RecipeInput, 
                     int time = Math.max(0, Mth.floor(recipeTimeFunction.applyAsDouble(baseTime)));
                     setTicksPerOperation(time);
                     shouldCheckCraftingTime = false;
-                    LTXIndustries.LOGGER.debug("Recomputed recipe process time for type {} to {}", LimaRegistryUtil.getNonNullRegistryId(this.recipeCheck.getRecipeType(), BuiltInRegistries.RECIPE_TYPE), time);
                 }
             }
 
@@ -340,13 +243,6 @@ public abstract class StateBlockRecipeMachineBlockEntity<I extends RecipeInput, 
     }
 
     @Override
-    protected void createConnectionCaches(ServerLevel level, Direction side)
-    {
-        super.createConnectionCaches(level, side);
-        fluidConnections.put(side, createCapabilityCache(Capabilities.FluidHandler.BLOCK, level, side));
-    }
-
-    @Override
     protected void onLoadServer(ServerLevel level)
     {
         super.onLoadServer(level);
@@ -358,18 +254,6 @@ public abstract class StateBlockRecipeMachineBlockEntity<I extends RecipeInput, 
     {
         super.loadAdditional(tag, registries);
         craftingProgress = tag.getInt(TAG_KEY_PROGRESS);
-
-        if (tag.contains(LimaCommonConstants.KEY_FLUID_TANKS, Tag.TAG_COMPOUND))
-        {
-            CompoundTag tanksTag = tag.getCompound(LimaCommonConstants.KEY_FLUID_TANKS);
-            for (BlockContentsType type : BlockContentsType.values())
-            {
-                LimaBlockEntityFluidHandler handler = getFluidHandler(type);
-                if (handler != null && tanksTag.contains(type.getSerializedName())) handler.deserializeNBT(registries, tanksTag.getList(type.getSerializedName(), Tag.TAG_COMPOUND));
-            }
-        }
-
-        if (fluidController != null) fluidController.deserializeNBT(registries, tag.getCompound(KEY_FLUID_IO));
     }
 
     @Override
@@ -377,16 +261,6 @@ public abstract class StateBlockRecipeMachineBlockEntity<I extends RecipeInput, 
     {
         super.saveAdditional(tag, registries);
         tag.putInt(TAG_KEY_PROGRESS, craftingProgress);
-
-        CompoundTag tanksTag = new CompoundTag();
-        for (BlockContentsType type : BlockContentsType.values())
-        {
-            LimaBlockEntityFluidHandler handler = getFluidHandler(type);
-            if (handler != null) tanksTag.put(type.getSerializedName(), handler.serializeNBT(registries));
-        }
-        if (!tanksTag.isEmpty()) tag.put(LimaCommonConstants.KEY_FLUID_TANKS, tanksTag);
-
-        if (fluidController != null) tag.put(KEY_FLUID_IO, fluidController.serializeNBT(registries));
     }
 
     private DoubleUnaryOperator createRecipeTimeFunction(DataComponentType<List<ValueUpgradeEffect>> type, LootContext context)
--- src/main/java/liedge/ltxindustries/blockentity/template/ProductionMachineBlockEntity.java
+++ src/main/java/liedge/ltxindustries/blockentity/template/ProductionMachineBlockEntity.java
@@ -1,28 +1,63 @@
 package liedge.ltxindustries.blockentity.template;
 
+import liedge.limacore.LimaCommonConstants;
 import liedge.limacore.blockentity.BlockContentsType;
+import liedge.limacore.capability.fluid.FluidHolderBlockEntity;
+import liedge.limacore.capability.fluid.LimaBlockEntityFluidHandler;
+import liedge.limacore.capability.fluid.LimaFluidUtil;
 import liedge.limacore.capability.itemhandler.LimaBlockEntityItemHandler;
+import liedge.ltxindustries.blockentity.base.BlockEntityInputType;
+import liedge.ltxindustries.blockentity.base.IOController;
 import liedge.ltxindustries.blockentity.base.SidedAccessBlockEntityType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.capabilities.BlockCapabilityCache;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import net.neoforged.neoforge.items.IItemHandler;
 import org.jetbrains.annotations.Nullable;
 
-public abstract class ProductionMachineBlockEntity extends EnergyMachineBlockEntity
-{
-    private final @Nullable LimaBlockEntityItemHandler inputInventory;
-    private final LimaBlockEntityItemHandler outputInventory;
+import java.util.EnumMap;
+import java.util.Map;
 
-    protected ProductionMachineBlockEntity(SidedAccessBlockEntityType<?> type, BlockPos pos, BlockState state, int auxInventorySize, int inputSlots, int outputSlots)
+public abstract class ProductionMachineBlockEntity extends EnergyMachineBlockEntity implements FluidHolderBlockEntity
+{
+    public static final int INPUT_TANK_CAPACITY = 32_000;
+    public static final int OUTPUT_TANK_CAPACITY = 64_000;
+
+    private final @Nullable LimaBlockEntityItemHandler inputInventory;
+    private final @Nullable LimaBlockEntityItemHandler outputInventory;
+
+    private final @Nullable LimaBlockEntityFluidHandler inputFluids;
+    private final @Nullable LimaBlockEntityFluidHandler outputFluids;
+    private final @Nullable IOController fluidController;
+    private final Map<Direction, BlockCapabilityCache<IFluidHandler, Direction>> fluidConnections = new EnumMap<>(Direction.class);
+    private int autoFluidOutputTimer = 0;
+
+    protected ProductionMachineBlockEntity(SidedAccessBlockEntityType<?> type, BlockPos pos, BlockState state, int auxInventorySize, int inputSlots, int outputSlots, int inputTanks, int outputTanks)
     {
         super(type, pos, state, null, auxInventorySize);
 
         this.inputInventory = inputSlots > 0 ? new LimaBlockEntityItemHandler(this, inputSlots, BlockContentsType.INPUT) : null;
-        this.outputInventory = new LimaBlockEntityItemHandler(this, outputSlots, BlockContentsType.OUTPUT);
+        this.outputInventory = outputSlots > 0 ? new LimaBlockEntityItemHandler(this, outputSlots, BlockContentsType.OUTPUT) : null;
+        this.inputFluids = inputTanks > 0 ? new LimaBlockEntityFluidHandler(this, inputTanks, BlockContentsType.INPUT) : null;
+        this.outputFluids = outputTanks > 0 ? new LimaBlockEntityFluidHandler(this, outputTanks, BlockContentsType.OUTPUT) : null;
+        this.fluidController = inputFluids != null || outputFluids != null ? new IOController(this, BlockEntityInputType.FLUIDS) : null;
     }
 
+    protected ProductionMachineBlockEntity(SidedAccessBlockEntityType<?> type, BlockPos pos, BlockState state, int auxInventorySize, int inputSlots, int outputSlots)
+    {
+        this(type, pos, state, auxInventorySize, inputSlots, outputSlots, 0, 0);
+    }
+
+    // Item stuff
     public LimaBlockEntityItemHandler getInputInventory()
     {
         return getItemHandlerOrThrow(BlockContentsType.INPUT);
@@ -30,7 +65,7 @@ public abstract class ProductionMachineBlockEntity extends EnergyMachineBlockEnt
 
     public LimaBlockEntityItemHandler getOutputInventory()
     {
-        return outputInventory;
+        return getItemHandlerOrThrow(BlockContentsType.OUTPUT);
     }
 
     @Override
@@ -67,5 +102,139 @@ public abstract class ProductionMachineBlockEntity extends EnergyMachineBlockEnt
     public @Nullable IItemHandler createItemIOWrapper(@Nullable Direction side)
     {
         return wrapInputOutputInventories(side);
+    }
+
+    // Fluid handling implementation
+    @Override
+    public @Nullable LimaBlockEntityFluidHandler getFluidHandler(BlockContentsType contentsType)
+    {
+        return switch (contentsType)
+        {
+            case INPUT -> inputFluids;
+            case OUTPUT -> outputFluids;
+            default -> null;
+        };
+    }
+
+    @Override
+    public int getBaseFluidCapacity(BlockContentsType contentsType, int tank)
+    {
+        return switch (contentsType)
+        {
+            case INPUT -> INPUT_TANK_CAPACITY;
+            case OUTPUT -> OUTPUT_TANK_CAPACITY;
+            default -> 0;
+        };
+    }
+
+    @Override
+    public int getBaseFluidTransferRate(BlockContentsType contentsType, int tank)
+    {
+        return getBaseFluidCapacity(contentsType, tank) / 4;
+    }
+
+    @Override
+    public boolean isValidFluid(BlockContentsType contentsType, int tank, FluidStack stack)
+    {
+        return true;
+    }
+
+    @Override
+    public @Nullable IFluidHandler createFluidIOWrapper(@Nullable Direction side)
+    {
+        return wrapInputOutputTanks(side);
+    }
+
+    @Override
+    protected IOController getFluidIOController()
+    {
+        return fluidController != null ? fluidController : super.getFluidIOController();
+    }
+
+    @Override
+    public void onFluidsChanged(BlockContentsType contentsType, int tank)
+    {
+        setChanged();
+    }
+
+    @Override
+    public @Nullable IFluidHandler getNeighborFluidHandler(Direction side)
+    {
+        return fluidConnections.get(side).getCapability();
+    }
+
+    protected void autoOutputFluids()
+    {
+        if (outputFluids != null && fluidController != null && fluidController.isAutoOutput())
+        {
+            for (Direction side : Direction.values())
+            {
+                if (fluidController.getSideIOState(side).allowsOutput())
+                {
+                    IFluidHandler neighborFluids = getNeighborFluidHandler(side);
+                    if (neighborFluids != null) LimaFluidUtil.transferFluidsFromLimaTank(outputFluids, neighborFluids, OUTPUT_TANK_CAPACITY, IFluidHandler.FluidAction.EXECUTE);
+                }
+            }
+        }
+    }
+
+    protected void autoOutputFluids(int frequency)
+    {
+        if (autoFluidOutputTimer >= frequency)
+        {
+            autoOutputFluids();
+            autoFluidOutputTimer = 0;
+        }
+        else
+        {
+            autoFluidOutputTimer++;
+        }
+    }
+
+    // Startup and Serialization
+    @Override
+    protected void createConnectionCaches(ServerLevel level, Direction side)
+    {
+        super.createConnectionCaches(level, side);
+
+        // Only create the fluid cache if we have fluid capabilities
+        if (inputFluids != null || outputFluids != null)
+        {
+            fluidConnections.put(side, createCapabilityCache(Capabilities.FluidHandler.BLOCK, level, side));
+        }
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries)
+    {
+        super.loadAdditional(tag, registries);
+
+        if (tag.contains(LimaCommonConstants.KEY_FLUID_TANKS, Tag.TAG_COMPOUND))
+        {
+            CompoundTag tanksTag = tag.getCompound(LimaCommonConstants.KEY_FLUID_TANKS);
+            for (BlockContentsType type : BlockContentsType.values())
+            {
+                LimaBlockEntityFluidHandler handler = getFluidHandler(type);
+                if (handler != null && tanksTag.contains(type.getSerializedName())) handler.deserializeNBT(registries, tanksTag.getList(type.getSerializedName(), Tag.TAG_COMPOUND));
+            }
+        }
+
+        if (fluidController != null) fluidController.deserializeNBT(registries, tag.getCompound(KEY_FLUID_IO));
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries)
+    {
+        super.saveAdditional(tag, registries);
+
+        CompoundTag tanksTag = new CompoundTag();
+        for (BlockContentsType type : BlockContentsType.values())
+        {
+            LimaBlockEntityFluidHandler handler = getFluidHandler(type);
+            if (handler != null) tanksTag.put(type.getSerializedName(), handler.serializeNBT(registries));
+        }
+        if (!tanksTag.isEmpty()) tag.put(LimaCommonConstants.KEY_FLUID_TANKS, tanksTag);
+
+        if (fluidController != null) tag.put(KEY_FLUID_IO, fluidController.serializeNBT(registries));
     }
 }
