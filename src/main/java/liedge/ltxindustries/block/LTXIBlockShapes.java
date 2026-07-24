6c987d13fdf5f231bb347c001902d4a331a3daa5
Ed J <cuttingedge237@gmail.com>
2026-07-23 23:30:08 -0500

    Bio/ARU Garden visual rework

--- src/main/java/liedge/ltxindustries/block/LTXIBlockShapes.java
+++ src/main/java/liedge/ltxindustries/block/LTXIBlockShapes.java
@@ -190,14 +190,12 @@ public final class LTXIBlockShapes
             Block.box(5, 17, 5, 11, 25, 11));
 
     public static final VoxelShape DIGITAL_GARDEN = Shapes.or(
-            // Base
-            Block.box(0, 0, 0, 16, 10, 16),
-            dimensionBox(0.5d, 10, 0.5d, 15, 3, 15),
-            // Back panel
-            dimensionBox(0.5d, 13, 13.5d, 15, 16, 2),
-            // Window
-            dimensionBox(1, 13, 1, 14, 16, 12.5d),
-            // Top
-            dimensionBox(0, 29, 0, 16, 3, 16));
+            dimensionBox(1, 0, 1, 14, 4, 14),
+            dimensionBox(2, 5, 2, 12, 3, 12),
+            dimensionBox(4, 4, 1, 8, 8, 1),
+            dimensionBox(1, 9, 1, 14, 3, 14),
+            dimensionBox(1.5d, 12, 1.5d, 13, 16, 13),
+            dimensionBox(1, 28, 1, 14, 3, 14),
+            dimensionBox(2, 31, 2, 12, 1, 12));
     //#endregion
 }
--- src/main/java/liedge/ltxindustries/client/LTXIndustriesClient.java
+++ src/main/java/liedge/ltxindustries/client/LTXIndustriesClient.java
@@ -245,7 +245,7 @@ public class LTXIndustriesClient
         @SubscribeEvent
         public void registerBlockColors(final RegisterColorHandlersEvent.BlockTintSources event)
         {
-            event.register(List.of(BlockTintSources.water()), LTXIBlocks.INFINITE_WATER_TANK.get(), LTXIBlocks.GEO_SYNTHESIZER.get(), LTXIBlocks.DIGITAL_GARDEN.get());
+            event.register(List.of(BlockTintSources.constant(0xff3f76e4)), LTXIBlocks.INFINITE_WATER_TANK.get(), LTXIBlocks.GEO_SYNTHESIZER.get(), LTXIBlocks.DIGITAL_GARDEN.get());
         }
 
         @SubscribeEvent
